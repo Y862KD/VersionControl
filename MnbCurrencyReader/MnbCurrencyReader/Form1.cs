f0d2302bcbb5880a581edeef08f6238d2432556b
Csaba Huzián <csaba.huzian@stud.uni-corvinus.hu>
2020-10-28 16:33:28 +0100

    Adatok megjelenítése diagramon

--- MnbCurrencyReader/MnbCurrencyReader/Form1.cs
+++ MnbCurrencyReader/MnbCurrencyReader/Form1.cs
@@ -9,40 +9,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
+using System.ServiceModel;
 
 namespace MnbCurrencyReader
 {
     public partial class Form1 : Form
     {
-        string currencyNames;
-        string startDate;
-        string endDate;
 
         BindingList<RateData> Rates = new BindingList<RateData>();
-        
 
         public Form1()
         {
             InitializeComponent();
 
+            MnbSoap();                
+                          
+            ChartDisplay();
+
+            dataGridView1.DataSource = Rates;
+        }
+
+        private void MnbSoap()
+        {
             var mnbService = new MNBArfolyamServiceSoapClient();
 
-            var request = new GetExchangeRatesRequestBody();
+            var request = new GetExchangeRatesRequestBody()
+
             {
-                currencyNames = "EUR";
-                startDate = "2020-01-01";
-                endDate = "2020-06-01";
-            }
+                currencyNames = "EUR",
+                startDate = "2020-01-01",
+                endDate = "2020-06-01"
+            };
 
             var response = mnbService.GetExchangeRates(request);
             var result = response.GetExchangeRatesResult;
 
+            XmlFeldolgozas(result);
+        }
 
-            dataGridView1.DataSource = Rates;
-
+        private void XmlFeldolgozas(string result)
+        {
             var xml = new XmlDocument();
             xml.LoadXml(result);
+
 
             foreach (XmlElement element in xml.DocumentElement)
             {
@@ -58,9 +69,28 @@ namespace MnbCurrencyReader
                 var value = decimal.Parse(childElement.InnerText);
                 if (unit != 0)
                     rate.Value = value / unit;
-
-      
             }
         }
+
+        private void ChartDisplay()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
+
+        }
+
     }
 }
