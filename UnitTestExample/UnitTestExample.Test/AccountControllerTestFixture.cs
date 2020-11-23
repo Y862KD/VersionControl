994f7f9b38f6d5bc1a4e12aaa1db23402214e3f8
Csaba Huzián <csaba.huzian@stud.uni-corvinus.hu>
2020-11-23 18:56:50 +0100

    E-mail validálás tesztesetek

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -10,14 +10,21 @@ namespace UnitTestExample.Test
 {
     public class AccountControllerTestFixture
     {
-        [Test]
+        [
+            Test,
+            TestCase("abcd1234", false),
+            TestCase("irf@uni-corvinus", false),
+            TestCase("irf.uni-corvinus.hu", false),
+            TestCase("irf@uni-corvinus.hu", true)
+        ]
+
         public void TestValidateEmail(string email, bool expectedResult)
         {
             //Arrange
-            var AccountController = new AccountController();
+            var accountController = new AccountController();
 
             //Act
-            var actualResult = AccountController.ValidateEmail(email);
+            var actualResult = accountController.ValidateEmail(email);
 
             //Assert
             Assert.AreEqual(expectedResult, actualResult);
