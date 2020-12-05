378e9ba81f5cb8549cded1a179b857b321e831d7
Csaba Huzián <csaba.huzian@stud.uni-corvinus.hu>
2020-12-05 11:56:17 +0100

    Győztes kiválasztása

--- EvoluciosAlgoritmus_Y862KD/EvoluciosAlgoritmus_Y862KD/Form1.cs
+++ EvoluciosAlgoritmus_Y862KD/EvoluciosAlgoritmus_Y862KD/Form1.cs
@@ -16,21 +16,31 @@ namespace EvoluciosAlgoritmus_Y862KD
         GameController gc = new GameController();
         GameArea ga;
 
+        //6. Új generáció
+        Brain winnerBrain = null;
+
+
+        //4. Induló populáció felépítése
         int populationSize = 100;
         int nbrOfSteps = 10;
         int nbrOfStepsIncrement = 10;
         int generation = 1;
 
+
+
         public Form1()
         {
             InitializeComponent();
 
+            //2. Játéktér
             ga = gc.ActivateDisplay();
             this.Controls.Add(ga);
 
             /*gc.AddPlayer();
             gc.Start(true);*/
 
+
+            //7. Iterációs lépés felépítése
             gc.GameOver += Gc_GameOver;
 
             for (int i = 0; i < populationSize; i++)
@@ -45,14 +55,33 @@ namespace EvoluciosAlgoritmus_Y862KD
 
         private void Gc_GameOver(object sender)
         {
+
+            //7. Iterációs lépés felépítése
             generation++;
             label1.Text = string.Format("{0}. generáció", generation);
 
+
+            //7.3 top performer
             var playerList = from p in gc.GetCurrentPlayers()
                              orderby p.GetFitness() descending
                              select p;
 
             var topPerformers = playerList.Take(populationSize / 2).ToList();
+
+            //7. Győztes kiválasztása
+
+            var winners = from p in topPerformers
+                          where p.IsWinner
+                          select p;
+            if (winners.Count() > 0)
+            {
+                winnerBrain = winners.FirstOrDefault().Brain.Clone();
+                gc.GameOver -= Gc_GameOver;
+                return;
+            }
+
+
+            //6. Új generáció
 
             gc.ResetCurrentLevel();
             foreach (var p in topPerformers)
